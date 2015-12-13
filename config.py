14917a46978f99452d0eeb45efe769c209e443b6
LobsterBandit <LobsterBandit@users.noreply.github.com>
2015-12-12 23:52:54 -0500

    Added TMDb api info and requests cache name/location

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
 
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
 SEARCHDIRS = ['E:\\1080p',
               'E:\\720p',
@@ -12,13 +13,20 @@ SEARCHDIRS = ['E:\\1080p',
               'X:\\720p',
               'X:\\3D',
               'V:\\High Def\\Movies\\1080p2']
-
 EXTENSIONS = ('.mkv', '.avi', '.mp4', '.iso', '.wmv')
+JSONFILE = 'Movies.txt'
 
-BASEDIR = os.path.abspath(os.path.dirname(__file__))
+# database information
 DBNAME = 'Movie_DB.db'
 DATABASE = os.path.join(BASEDIR, DBNAME)
 
-JSONFILE = 'Movies.txt'
+# TMDb api information
+APIURL = 'https://api.themoviedb.org/3/'
+APIKEY = os.environ['TMDBAPIKEY']
+HEADERS = {
+    'Accept': 'application/json'
+}
 
-
+# requests cache
+CACHENAME = 'tmdb_cache'
+CACHELOC = os.path.join(BASEDIR, CACHENAME)
