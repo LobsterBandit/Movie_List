4c2cdc9ab17b49d516f1c473d1ee498afb64c5ec
LobsterBandit <LobsterBandit@users.noreply.github.com>
2016-01-03 17:03:42 -0500

    Added insert or replace command for Video table

--- update_db.py
+++ update_db.py
@@ -29,3 +29,28 @@ def upsert_db(movie_list, database=DATABASE):
         raise e
     finally:
         db.close()
+
+
+def upsert_videos(video_list, database=DATABASE):
+    try:
+        db = sqlite3.connect(database)
+        cur = db.cursor()
+
+        for video in video_list:
+            cur.execute("""
+            insert or replace into Video (Video_ID, Type, Key, Name, Size, Site, Movie_id, Date_Added)
+
+            values ((select Video_ID from Video where Movie_id = ? and Key = ?),
+                    ?,?,?,?,?,?,
+                    (select Date_Added from Video where Movie_id = ? and Key = ?))
+            """, (video['movie_id'], video['key'], video['type'], video['key'], video['name'], video['size'],
+                  video['site'], video['movie_id'], video['movie_id'], video['key'])
+            )
+
+        db.commit()
+
+    except Exception as e:
+        db.rollback()
+        raise e
+    finally:
+        db.close()
