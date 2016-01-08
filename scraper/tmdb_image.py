eef070e58ff463450983eaec6b335a1ac1299feb
LobsterBandit <LobsterBandit@users.noreply.github.com>
2016-01-07 22:01:18 -0500

    Added scan of static/images folder to avoid downloading images more than once

--- scraper/tmdb_image.py
+++ scraper/tmdb_image.py
@@ -40,6 +40,16 @@ def download_images(movieobj, base_url, size, output, poster=True, backdrop=True
             print('{} created at {}.'.format(filename, output))
 
 
+def scan_static_img(loc=APPIMG):
+    imglist = []
+    for root, dirs, files in os.walk(loc):
+        for file in files:
+            imgfile = '{}_{}_{}'.format(file.split('_')[0], file.split('_')[1], file.split('_')[2][:-4])
+            print(imgfile)
+            imglist.append(imgfile)
+    return imglist
+
+
 if __name__ == '__main__':
     config = _get_json(CONFIG_PATTERN.format(key=APIKEY))
     base_url = config['images']['secure_base_url']
@@ -51,9 +61,30 @@ if __name__ == '__main__':
     session = scoped_session(sessionmaker(bind=engine))
 
     # all images
-    # movies = session.query(MovieList).order_by(MovieList.Title).all()
+    movies = session.query(MovieList).order_by(MovieList.Title).all()
 
+    imgs = scan_static_img()
+
+    getlist = []
+    for movie in movies:
+        poster = '{title}_{year}_poster'.format(title=movie.Title, year=movie.Year)
+        backdrop = '{title}_{year}_backdrop'.format(title=movie.Title, year=movie.Year)
+        if poster.lower() in [img.lower() for img in imgs]:
+            getposter = False
+            print('{} poster already downloaded'.format(movie.Title))
+        else:
+            getposter = True
+        if backdrop.lower() in [img.lower() for img in imgs]:
+            getbackdrop = False
+            print('{} backdrop already downloaded'.format(movie.Title))
+        else:
+            getbackdrop = True
+        if getposter or getbackdrop:
+            getlist.append(movie)
+            print('Queued {} for download'.format(movie.Title))
+
+    # print(getlist)
     # one image based on imdb_id
-    movies = session.query(MovieList).filter(MovieList.imdb_id=='tt2503944').all()
+    # getlist = session.query(MovieList).filter(MovieList.imdb_id=='tt2503944').all()
 
-    download_images(movies, base_url, size, output)
+    download_images(getlist, base_url, size, output)
